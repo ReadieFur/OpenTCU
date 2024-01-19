2289835709c81a124c8532bc8020d536472515b6
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-01-19 04:52:43 +0000

    Fix SpiCan halting

--- OpenTCU/src/CAN/ACan.hpp
+++ OpenTCU/src/CAN/ACan.hpp
@@ -7,9 +7,10 @@
 #include <freertos/portmacro.h>
 #include "SCanMessage.hpp"
 
+//TODO: Add concurrency safety.
 class ACan
 {
 public:
     virtual esp_err_t Send(SCanMessage message, TickType_t timeout = 0) = 0;
-    virtual esp_err_t Receive(SCanMessage &message, TickType_t timeout = 0) = 0;
+    virtual esp_err_t Receive(SCanMessage* message, TickType_t timeout = 0) = 0;
 };
