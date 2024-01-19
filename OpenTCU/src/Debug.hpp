85467a1c84c5e0fe1ca1c8883bffcc13d5fc8a35
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-01-19 17:38:17 +0000

    Misc changes

--- OpenTCU/src/Debug.hpp
+++ OpenTCU/src/Debug.hpp
@@ -18,7 +18,6 @@ class Debug
 private:
     static bool _initialized;
     static AsyncWebServer* _debugServer;
-    
 
 public:
     static void Setup()
@@ -28,18 +27,45 @@ public:
         _initialized = true;
 
         _debugServer = new AsyncWebServer(81);
+
+        IPAddress ipAddress;
         WiFi.mode(WIFI_STA);
         WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
         if (WiFi.waitForConnectResult() != WL_CONNECTED)
         {
-            vTaskDelay(5000 / portTICK_PERIOD_MS);
-            printf("WiFi Failed!\n");
-            vTaskDelete(NULL);
+            printf("STA Failed!\n");
+
+            //Create AP instead.
+            WiFi.mode(WIFI_AP);
+            //Create AP using mac address.
+            uint8_t mac[6];
+            WiFi.macAddress(mac);
+            char ssid[18];
+            sprintf(ssid, "ESP32-%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+            WiFi.softAP(ssid);
+            ipAddress = WiFi.softAPIP();
         }
+        else
+        {
+            ipAddress = WiFi.localIP();
+        }
+
         WebSerial.begin(_debugServer);
         _debugServer->begin();
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-        printf("Debug server started at %s\n", WiFi.localIP().toString().c_str());
+
+        printf("Debug server started at %s\n", ipAddress.toString().c_str());
+    }
+
+    static void Blip(int count = 1)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            digitalWrite(LED_PIN, ON);
+            delay(100);
+            digitalWrite(LED_PIN, OFF);
+            if (i < count - 1)
+                delay(100);
+        }
     }
 };
 
