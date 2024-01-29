d8363c82fc85aa9d80e40e6e350944e22c353c96
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-01-29 01:50:36 +0000

    Trying without driver lock

--- OpenTCU/src/CAN/ACan.h
+++ OpenTCU/src/CAN/ACan.h
@@ -7,10 +7,14 @@
 #include <freertos/semphr.h>
 #include "SCanMessage.h"
 
+// #define USE_DRIVER_LOCK
+
 class ACan
 {
 protected:
+    #ifdef USE_DRIVER_LOCK
     volatile SemaphoreHandle_t driverMutex = xSemaphoreCreateMutex();
+    #endif
 public:
     virtual esp_err_t Send(SCanMessage message, TickType_t timeout = 0) = 0;
     virtual esp_err_t Receive(SCanMessage* message, TickType_t timeout = 0) = 0;
