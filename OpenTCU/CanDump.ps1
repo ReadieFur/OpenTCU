0d0191079ff74317edc831d3db86227a6986696c
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-01-29 16:15:18 +0000

    Record data

--- OpenTCU/CanDump.ps1
+++ OpenTCU/CanDump.ps1
@@ -15,7 +15,8 @@ $outputFilePath = Join-Path $folderPath $randomFileName
 Write-Host "Filtering output to: $outputFilePath"
 
 # Start the command and stream the output to the terminal
-pio remote device monitor -p COM9 -b 115200 | ForEach-Object {
+pio device monitor --environment esp32c3 | ForEach-Object {
+# pio remote device monitor -p COM9 -b 115200 | ForEach-Object {
     # Check if the line starts with "[CAN]"
     if ($_ -match "^\[CAN\]") {
         # Remove the "[CAN]" prefix
