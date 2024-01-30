d250afe8191378ae2f37e64236a5ec42bacbc2a1
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-01-30 04:45:06 +0000

    Moved SmartLeds to platformio dev profile

--- OpenTCU/components/SmartLeds-3.1.2/src/SmartLeds.cpp
+++
@@ -1,9 +0,0 @@
-#include "SmartLeds.h"
-
-IsrCore SmartLed::_interruptCore = CoreCurrent;
-
-SmartLed*& IRAM_ATTR SmartLed::ledForChannel(int channel) {
-    static SmartLed* table[detail::CHANNEL_COUNT] = {};
-    assert(channel < detail::CHANNEL_COUNT);
-    return table[channel];
-}
