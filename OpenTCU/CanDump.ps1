da9d5af5f4f328d72690bc727ebb210620285c1c
Chloe (Tristan) Read <60425965+ReadieFur@users.noreply.github.com>
2024-07-07 11:48:22 +0100

    Update candumper

--- OpenTCU/CanDump.ps1
+++ OpenTCU/CanDump.ps1
@@ -8,14 +8,14 @@ if (-not (Test-Path $folderPath)) {
 
 # Generate a random file name with a timestamp
 $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-$randomFileName = "output_$timestamp.txt"
+$randomFileName = "output_$timestamp.candump"
 $outputFilePath = Join-Path $folderPath $randomFileName
 
 # Display the generated file path
 Write-Host "Filtering output to: $outputFilePath"
 
 # Start the command and stream the output to the terminal
-pio device monitor --environment esp32c3 | ForEach-Object {
+pio device monitor --environment esp32c3-debug | ForEach-Object {
 # pio remote device monitor -p COM9 -b 115200 | ForEach-Object {
     # Check if the line starts with "[CAN]"
     if ($_ -match "^\[CAN\]") {
