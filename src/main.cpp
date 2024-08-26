5dc77617b91cfb7959b08836e87417655edf5800
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-08-26 16:58:12 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,96 @@
 #define REED_POSITIVE GPIO_NUM_0
 #define REED_NEGATIVE GPIO_NUM_1
 #define RELAY_BASE GPIO_NUM_2
-#define TIMEOUT 3 * 1000000UL //Timeout of x seconds (in microseconds).
+#define TIMEOUT 3 * 1000 //Timeout of x seconds (in milliseconds).
 #define MIN_RPM static_cast<int>((ACTIVATION_SPEED * 1000 / 60) / (WHEEL_CIRCUMFERENCE / 1000)) //Minimum RPM for applying the multiplier (round down). Convert speed to m/min, convert WHEEL_CIRCUMFERENCE to meters, divide the two for RPM.
 #define MAX_RPM static_cast<int>((DEACTIVATION_SPEED * 1000 / 60) / (WHEEL_CIRCUMFERENCE / 1000) + 1) //Maximum RPM for applying the multiplier (round up, +1 rounds up with static_cast<int> truncating any decimal, assuming there is a decimal).
 
 ulong lastLogTime = 0;
+volatile int lastState = LOW;
+volatile ulong onTime = 0;
+volatile ulong offTime = 0;
+volatile ulong onDuration = 0;
+volatile ulong offDurationTmp = 0;
+volatile ulong offDuration = 0;
+volatile ulong lastDurationUpdate = 0;
+void IRAM_ATTR ReadISR()
+{
+	ulong now = millis();
+	int state = digitalRead(REED_NEGATIVE);
+
+	if (lastState == LOW && state == HIGH)
+	{
+		offDurationTmp = now - offTime;
+		onTime = now;
+	}
+	else if (lastState == HIGH && state == LOW)
+	{
+		onDuration = now - onTime;
+		offDuration = offDurationTmp;
+		lastDurationUpdate = now;
+		offTime = now;
+	}
+
+	lastState = state;
+}
+
+void RelayTask(void* params)
+{
+	while (true)
+	{
+		//Capture variables (as soon as possible without a mutex).
+		ulong _onDuration = onDuration;
+		ulong _offDuration = offDuration;
+		ulong _lastDurationUpdate = lastDurationUpdate;
+
+		ulong now = millis();
+
+		if (now - _lastDurationUpdate > 3000)
+		{
+			if (now - lastLogTime > 1000)
+			{
+				std::cout
+					<< "Idle"
+					<< std::endl;
+
+				lastLogTime = now;
+			}
+
+			vTaskDelay(0);
+			continue;
+		}
+
+		double totalCycleDuration = (_onDuration + _offDuration) / 1000.0;
+		double rpm = 60.0 / totalCycleDuration;
+
+		bool applyMultiplier = (rpm >= MIN_RPM && rpm <= MAX_RPM);
+		ulong modifiedOnDuration = applyMultiplier ? _onDuration * SPEED_MULTIPLIER : _onDuration;
+		ulong modifiedOffDuration = applyMultiplier ? _offDuration * SPEED_MULTIPLIER : _offDuration;
+
+		digitalWrite(RELAY_BASE, HIGH);
+		vTaskDelay(pdMS_TO_TICKS(modifiedOnDuration));
+
+		digitalWrite(RELAY_BASE, LOW);
+		vTaskDelay(pdMS_TO_TICKS(modifiedOffDuration));
+
+		if (now - lastLogTime > 1000)
+		{
+			std::cout
+				<< "On Duration: " << (_onDuration) << "ms"
+				<< ", Off Duration: " << (_offDuration) << "ms"
+				<< ", RPM: " << rpm
+				<< ", Apply Multiplier: " << applyMultiplier
+				<< ", Fake On Duration: " << (modifiedOnDuration) << "ms"
+				<< ", Fake Off Duration: " << (modifiedOffDuration) << "ms"
+				<< ", Fake RPM: " << (applyMultiplier ? rpm / SPEED_MULTIPLIER : rpm)
+				<< std::endl;
+
+			lastLogTime = now;
+		}
+
+		vTaskDelay(0);
+	}
+}
 
 void setup()
 {
@@ -25,63 +110,12 @@ void setup()
 	pinMode(RELAY_BASE, OUTPUT);
 	pinMode(GPIO_NUM_3, INPUT_PULLDOWN);
 	digitalWrite(REED_POSITIVE, HIGH);
+
+	attachInterrupt(REED_NEGATIVE, ReadISR, CHANGE);
+	xTaskCreate(RelayTask, "RelayTask", configMINIMAL_STACK_SIZE + 4096, NULL, 5, NULL);
 }
 
 void loop()
 {
-	//Measure the duration of the HIGH signal.
-	ulong highDuration = pulseIn(REED_NEGATIVE, HIGH, TIMEOUT);
-	
-	//Measure the duration of the LOW signal.
-	ulong lowDuration = pulseIn(REED_NEGATIVE, LOW, TIMEOUT);
-
-	//If a timeout occurs, pulseIn() returns 0.
-	if (highDuration > 0 && lowDuration > 0)
-	{
-		//Calculate the total cycle duration in microseconds.
-		ulong totalCycleDuration = highDuration + lowDuration;
-		//Calculate RPM.
-		float totalCycleDurationSeconds = totalCycleDuration / 1000000.0; //Convert to seconds.
-		float rpm = 60.0 / totalCycleDurationSeconds; //RPM = 60 / (Total Cycle Duration in seconds).
-		//Determine if the RPM is within the specified range.
-		bool applyMultiplier = (rpm >= MIN_RPM && rpm <= MAX_RPM);
-
-		//Calculate the modified durations.
-		ulong modifiedHighDuration = applyMultiplier ? highDuration * SPEED_MULTIPLIER : highDuration;
-		ulong modifiedLowDuration = applyMultiplier ? lowDuration * SPEED_MULTIPLIER : lowDuration;
-
-		//Relay the signal with the modified timing.
-		digitalWrite(RELAY_BASE, HIGH);
-		delayMicroseconds(modifiedHighDuration);
-		
-		digitalWrite(RELAY_BASE, LOW);
-		delayMicroseconds(modifiedLowDuration);
-
-		ulong now = millis();
-		if (now - lastLogTime >= 1000UL)
-		{
-			//Log the accumulated on and off times in milliseconds.
-			std::cout
-				<< "On Duration: " << (highDuration / 1000.0) << "ms"
-				<< ", Off Duration: " << (lowDuration / 1000.0) << "ms"
-				<< ", RPM: " << rpm
-				<< ", Apply Multiplier: " << applyMultiplier
-				<< ", Fake On Duration: " << (modifiedHighDuration / 1000.0) << "ms"
-				<< ", Fake Off Duration: " << (modifiedLowDuration / 1000.0) << "ms"
-				<< ", Fake RPM: " << (applyMultiplier ? rpm / SPEED_MULTIPLIER : rpm)
-				<< std::endl;
-			
-			lastLogTime = now;
-		}
-	}
-	else
-	{
-		ulong now = millis();
-		if (now - lastLogTime >= 1000UL)
-		{
-			std::cout << "Idle " << (highDuration > 0 ? highDuration / 1000 : highDuration)
-				<< " " << (lowDuration > 0 ? lowDuration / 1000 : lowDuration) << std::endl;
-			lastLogTime = now;
-		}
-	}
+	vTaskDelete(NULL);
 }
