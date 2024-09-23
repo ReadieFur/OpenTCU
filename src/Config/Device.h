3f2e441d7f6f5ab3ce908ce837d238543ed9d6d0
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-09-24 00:04:06 +0100

    Update BMS code

--- src/Config/Device.h
+++ src/Config/Device.h
@@ -5,3 +5,7 @@
 
 #define TCU_NAME        ""          //The bluetooth name that the TCU appears as.
 #define TCU_PIN         0000        //The pin to connect to the TCU.
+
+#define BAT_CHG         4100        //The voltage (in millivolts) that the device runs at when plugged in.
+#define BAT_LOW         3700        //The voltage (in millivolts) for low battery.
+#define BAT_CRIT        3600        //The voltage (in millivolts) for critical battery levels.
