41053e8d4059cca3e5339dc0932ec5a20ae4d042
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-09-23 21:47:26 +0100

    Misc changes

---
+++ src/Common.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define nameof(var) #var
+
+#include <esp_check.h>
+
+#include "Pinout.h"
+#include "StaticConfig.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
 #include <freertos/FreeRTOS.h> //Has to always be the first included FreeRTOS related header.
 #include "StaticConfig.h"
 #include "CAN/BusMaster.hpp"
-#include "Power/PowerManager.hpp"
-#include "Bluetooth/BluetoothMaster.hpp"
 
 static_assert(ESP_NAME[0] != '\0', "ESP_NAME cannot be empty.");
 
+BusMaster* busMaster;
+
 void setup()
 {
-    //It is critical that the BusMaster is the first service to be started.
-    BusMaster::Begin();
-    PowerManager::Begin();
-    BluetoothMaster::Begin();
+    busMaster = new BusMaster();
+    busMaster->InstallService();
 }
 
 void loop()
