0bebec20eccfcafd6526d8200fd9b37d4fef6455
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-09-23 21:47:26 +0100

    Misc changes

--- src/StaticConfig.h
+++ src/StaticConfig.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define ESP_NAME        "ESP_TCU"   //The name this device should appear as under bluetooth settings.
+#define ESP_NAME        "ESP_TCU"   //The name this device should appear as under bluetooth settings. TODO: Make this the TCU name with the prefix replaced with ESP.
 #define ESP_PIN         ""          //The pin that should be used when pairing with this device.
 
 #define TCU_NAME        ""          //The bluetooth name that the TCU appears as.
