6626e9589984f195e8fc1d488a1b84eca1605115
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-09-24 16:16:15 +0100

    Update bluetooth

--- src/Bluetooth/API.hpp
+++ src/Bluetooth/API.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
+#include <BLEServer.h>
+
 namespace ReadieFur::OpenTCU::Bluetooth
 {
     class API
     {
     private:
+        BLEServer& _server;
+
     public:
+        API(BLEServer& server) : _server(server) {}
     };
 };
