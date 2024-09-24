a6de2d161879b673944c2979f7f06e08f4a43617
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-09-24 16:02:12 +0100

    Update AService

--- src/Abstractions/EServiceResult.h
+++ src/Abstractions/EServiceResult.h
@@ -5,6 +5,9 @@ namespace ReadieFur::Abstractions
     enum EServiceResult
     {
         Ok = 0,
-        NotInstalled = -1
+        NotInstalled = -1,
+        InUse = -2,
+        MissingDependencies = -3,
+        DependencyNotReady = -4
     };
 };
