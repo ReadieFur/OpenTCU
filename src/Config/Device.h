3e809af33e39dd5112f181dffd043b64382e2614
Chloe (Tristan) Read <60425965+ReadieFur@users.noreply.github.com>
2024-11-28 15:12:30 +0000

    Updated device name

--- src/Config/Device.h
+++ src/Config/Device.h
@@ -1,15 +1,4 @@
 #pragma once
 
-#define DEVICE_NAME     "OpenTCU001130512T"    //The name of the device.
-
 #define TCU_NAME        "WSBC001130512T"//The bluetooth name that the TCU appears as.
 #define TCU_PIN         "0000"      //The pin to connect to the TCU.
-
-#define BAT_CHG         4100        //The voltage (in millivolts) that the device runs at when plugged in.
-#define BAT_LOW         3700        //The voltage (in millivolts) for low battery.
-#define BAT_CRIT        3600        //The voltage (in millivolts) for critical battery levels.
-#define BAT_CRIT_INT    300000      //The time (in milliseconds) for the module to sleep before waking up again.
-
-#define GSM_BAUD        115200      //GSM module baud rate.
-
-#define GPS_BAUD        9600
