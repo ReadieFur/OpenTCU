8ad75e1319423b158738f0fc3d9d94b2b10f5888
Chloe (Tristan) Read <60425965+ReadieFur@users.noreply.github.com>
2024-12-09 00:43:48 +0000

    Removed test code

--- Software/src/Bluetooth/API.hpp
+++ Software/src/Bluetooth/API.hpp
@@ -34,28 +34,6 @@ namespace ReadieFur::OpenTCU::Bluetooth
 
             BLE* bleService = GetService<BLE>();
 
-            //https://www.gofakeit.com/funcs/uint32
-            //https://www.rapidtables.com/convert/number/decimal-to-hex.html
-            GattServerService testService(SUUID(0x98C220BEUL), 0);
-            uint16_t testValue = 53;
-            uint16_t testValue2 = 25;
-            testService.AddAttribute(SUUID(0xCAE35F80UL), ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE, &testValue, sizeof(testValue), sizeof(uint16_t));
-            testService.AddAttribute(SUUID(0x38B18BECUL), ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE, sizeof(uint16_t),
-            [testValue2](auto outValue, auto outLength)
-            {
-                LOGD(nameof(Bluetooth::API), "Manual read callback.");
-                *outValue = testValue2;
-                *outLength = sizeof(testValue2);
-                return ESP_GATT_OK;
-            },
-            [&testValue2](auto inValue, auto inLength)
-            {
-                LOGD(nameof(Bluetooth::API), "Manual write callback.");
-                testValue2 = *(uint16_t*)inValue;
-                return ESP_GATT_OK;
-            });
-            _services.push_back(&testService);
-
             if ((err = bleService->RegisterServerApp(&_serverProfile)) != ESP_OK)
             {
                 LOGE(nameof(Bluetooth::API), "Failed to register server app: %s", esp_err_to_name(err));
@@ -63,12 +41,6 @@ namespace ReadieFur::OpenTCU::Bluetooth
             }
 
             LOGD(nameof(Bluetooth::API), "BLE API started.");
-
-            //Test external notification.
-            vTaskDelay(pdMS_TO_TICKS(15000));
-            testValue2 = 42;
-            LOGI(nameof(Bluetooth::API), "Sending notification.");
-            esp_ble_gatts_send_indicate(_serverProfile.gattsIf, _serverProfile.connectionId, testService.GetAttributeHandle(SUUID(0x38B18BECUL)), sizeof(testValue2), (uint8_t*)&testValue2, false);
 
             ServiceCancellationToken.WaitForCancellation();
 
