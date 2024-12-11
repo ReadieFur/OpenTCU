4d745a07d4260ac959c33e5a4507a761ab9f5fde
Chloe (Tristan) Read <60425965+ReadieFur@users.noreply.github.com>
2024-12-11 07:53:11 +0000

    Dynamically set device name

--- Software/src/Config/Device.h
+++ Software/src/Config/Device.h
@@ -1,4 +1,3 @@
 #pragma once
 
-#define TCU_NAME        "WSBC001130512T"    //The bluetooth name that the TCU appears as.
 #define TCU_PIN         123456              //The pin to connect to the TCU.
