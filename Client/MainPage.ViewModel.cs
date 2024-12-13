1e0ca68cc7cf264a5ba91370355a1a67ded52c5f
Chloe (Tristan) Read <60425965+ReadieFur@users.noreply.github.com>
2024-12-13 09:56:23 +0000

    Upload WIP client app code

---
+++ Client/MainPage.ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadieFur.OpenTCU.Client
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private int _baseCircumference = 2160;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int BaseCircumference
+        {
+            get => _baseCircumference;
+            set
+            {
+                if (BaseCircumference != value)
+                {
+                    //Don't check value ranges here as it can cause issues with text input, only validate it externally upon final submission.
+                    /*if (value > 2400)
+                        _baseCircumference = 2400;
+                    else if (value < 2000)
+                        _baseCircumference = 2000;
+                    else*/
+                        _baseCircumference = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
