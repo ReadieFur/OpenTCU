716acdbfb145a1a8b69a67076803d8f6a15918cc
Chloe Read <dev@readiefur.com>
2026-04-14 23:32:58 +0100

    Update ProgramConfig.h

--- Software/src/ProgramConfig.h
+++ Software/src/ProgramConfig.h
@@ -41,7 +41,7 @@
 // #define CAN_DUMP_SERIAL
 #define CAN_DUMP_UDP
 #define CAN_DUMP_BEFORE_INTERCEPT
-#define CAN_DUMP_BLANK_ON_TIMEOUT
+// #define CAN_DUMP_BLANK_ON_TIMEOUT
 #endif
 
 // ==== Conditionals ====
